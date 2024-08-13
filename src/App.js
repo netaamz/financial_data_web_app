35827abb9c1a7763140c4803257f85f94e1866ef
netaamz <netaamz@gmail.com>
2024-08-13 14:38:49 +0300

    added coinitem

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import CoinMarketCap from "./components/CoinMarketCap.js";
+import Coins from "./components/Coins.js";
 function App() {
-  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=10';
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=10";
   const [coins, setCoins] = useState([]);
 
   useEffect(() => {
@@ -16,9 +17,11 @@ function App() {
         console.log(error);
       });
   }, []);
-  return   <>
-    
-</>;
+  return (
+    <>
+      <Coins coins={coins}></Coins>
+    </>
+  );
 }
 
 export default App;
--- src/components/Coins.js
+++ src/components/Coins.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import CoinItem from './CoinItem'
 
-const Coins = () => {
+const Coins = (props) => {
   return (
-    <div>Coins</div>
+    <div className='container'>
+        <div>
+            <div className='heading'>
+                <p>#</p>
+                <p className='coin-name'>Coin</p>
+                <p>Price</p>
+                <p>24h</p>
+                <p className='hide-mobile'>Volume</p>
+                <p className='hide-mobile'>Mkt Cap</p>
+            </div>
+            {props.coins.map( coins =>{
+                return(<CoinItem  coins={coins}/>)
+            })}
+        </div>
+    </div>
   )
 }
 
