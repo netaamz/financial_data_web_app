4f01de9de66d962e048f41c455a925025e10d375
netaamz <netaamz@gmail.com>
2024-08-13 13:54:17 +0300

    setting up

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import CoinMarketCap from "./components/CoinMarketCap.js";
+function App() {
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=10';
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(url)
+      .then((response) => {
+        setCoins(response.data);
+        console.log(response.data[0]);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+  return   <>
+    
+</>;
+}
+
+export default App;
