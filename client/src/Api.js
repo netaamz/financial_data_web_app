068cbfd96acaeb2df1b42ed220eee418845f7a69
netaamz <netaamz@gmail.com>
2024-08-29 19:17:19 +0300

    Delete Api.js

--- client/src/Api.js
+++
@@ -1,7 +0,0 @@
-const API_URL = process.env.REACT_APP_API_URL;
-
-export const fetchData = async () => {
-  const response = await fetch(`${API_URL}/api/endpoint`);
-  const data = await response.json();
-  return data;
-};
